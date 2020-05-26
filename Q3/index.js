47a3afc8e9644bb9ba836a91f60e1a9677873b0d
Abduliitr <abdul.thomso@gmail.com>
2020-05-27 04:34:22 +0530

    Q2 | Q3 | Q4

---
+++ Q3/index.js
@@ -0,0 +1,8 @@
+function myFunction(num1, num2) {
+    if (num1 > num2) {
+        alert("First parameter ("+num1+") is greater than the second ("+num2+")!");
+        
+    }else{
+        return (num1+num2);
+    }
+}
